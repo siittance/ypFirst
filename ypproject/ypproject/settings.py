e2cf11d1a4865b70bfbec3d5b2385fda314c6b84
siittance <144900566+siittance@users.noreply.github.com>
2025-06-21 04:11:56 +0300

    last1

--- ypproject/ypproject/settings.py
+++ ypproject/ypproject/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'ypproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'NAME': 'ypprojectDB',
-        'USER': 'postgres',
-        'PASSWORD': '1'
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'PORT': '3306',
+        'NAME': '$ypprojectDB',
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
